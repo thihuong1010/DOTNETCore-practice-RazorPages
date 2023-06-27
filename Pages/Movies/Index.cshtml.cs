cd6d1f9667972832d6bb7a795d57c53dbd8bef05
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-06-27 22:02:44 +0300

    Add search to ASP.NET Core Razor Pages
    - Search by title
    - Search by genre

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace RazorPagesMovie.Pages.Movies
 {
@@ -21,12 +22,36 @@ namespace RazorPagesMovie.Pages.Movies
 
         public IList<Movie> Movie { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)] // is required for binding on HTTP GET request
+        // Text users enter in the search text box
+        public string? SearchString { get; set; } 
+
+        // Genres contains the list of genres
+        public SelectList? Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        // The genre that user selects
+        public string? MovieGenre { get; set; }
+
         public async Task OnGetAsync()
         {
-            if (_context.Movie != null)
+            // Use LINQ to get list of genres
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
             {
-                Movie = await _context.Movie.ToListAsync();
+                movies = movies.Where(s => s.Title.Contains(SearchString));
             }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
