b31cdbd116e119e007d82c3dab595796f3b85501
Huong Nguyen <65859562+thihuong1010@users.noreply.github.com>
2023-06-28 21:24:27 +0300

    Combine attributes on one line
    Make validation rules at Model in one line

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -8,27 +8,20 @@ namespace RazorPagesMovie.Models
         public int Id { get; set; }
 
         [StringLength(60, MinimumLength = 3)]
-        [Required]
         public string? Title { get; set; } = string.Empty;
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
-        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
-        [Required]
-        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
         public string Genre { get; set; } = string.Empty;
 
-        [Range(1, 100)]
-        [DataType(DataType.Currency)]
+        [Range(1, 100), DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
         // Add Rating property to the Movie model
-        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-        [StringLength(5)]
-        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
         public string Rating { get; set; } = string.Empty;
     }
 }
